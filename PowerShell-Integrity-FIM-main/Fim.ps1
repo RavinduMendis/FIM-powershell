afd907d663949440dbc55cacda341f0e70107a0c
ravindu malshan <53220147+RavinduMendis@users.noreply.github.com>
2024-02-04 23:22:26 +0530

    Update Fim.ps1

--- PowerShell-Integrity-FIM-main/Fim.ps1
+++ PowerShell-Integrity-FIM-main/Fim.ps1
@@ -1,8 +1,12 @@
-﻿function hashing($path, $algorithm = "SHA512"){
+#functions
+
+#hashing function
+function hashing($path, $algorithm = "SHA512"){
     $fileHash = Get-FileHash -Path $path -Algorithm $algorithm
     return $fileHash
 }
 
+#delete baseline if exists
 function deleteBaseline(){
     $baselineExists = Test-Path -Path .\baseline.txt
     if ($baselineExists){
@@ -11,12 +15,14 @@ function deleteBaseline(){
     }
 }
 
+#log message
 function logMessage($message, $color){
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     Write-Host "$timestamp - $message" -ForegroundColor $color
 }
 
-cls
+#main script
+cls #clear terminal
 Write-Host ""
 Write-Host "What would you like to do?"
 Write-Host ""
@@ -24,34 +30,42 @@ Write-Host "    A) Collect new Baseline?"
 Write-Host "    B) Begin monitoring files with saved Baseline?"
 Write-Host ""
 Write-Host ""
-$path = ".\Files"
+$path = ".\Files" #change file path
 
+
+#main loop
 while($true){
     $response = Read-Host -Prompt "Please enter 'A' or 'B'"
     $response = $response.ToUpper()
 
     if($response -eq "A"){
-        deleteBaseline
+        deleteBaseline #calling delete baseline funcion
 
-        $files = Get-ChildItem -Path $path -Recurse
+        $files = Get-ChildItem -Path $path -Recurse   #gather files
+
+        #hashing and store in baseline file
         foreach($item in $files){
             $hash = hashing -path $item.FullName
             "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath ".\baseline.txt" -Append
             $item
         }
 
+        #printing output
         Write-Host ""
         Write-Host "Algorithm - $($hash.Algorithm)" -ForegroundColor DarkCyan
         Write-Host ""
         logMessage "Baseline collected." Green
         break
     }
+
     elseif($response -eq "B"){
+        #monitoring
         cls
         Write-Host ""
         Write-Host "Start monitoring (CTRL + C to exit)"
         Write-Host ""
 
+        #store baseline file to dictionary
         $baselineDictionary = @{}
         $baselineContent = Get-Content -Path .\baseline.txt
         foreach($data in $baselineContent){
@@ -63,16 +77,20 @@ while($true){
             foreach($file in $files){
                 $hash = hashing -path $file.FullName
 
+                #checking file has been created
                 if (-not [string]::IsNullOrEmpty($hash.Path)){
                     if($baselineDictionary[$hash.Path] -eq $null){
                         logMessage "$($hash.Path) has been created!" Green
                     }
+
+                    #checking file has been edited
                     elseif($baselineDictionary[$hash.Path] -ne $hash.Hash ){
                         logMessage "$($hash.Path) has been edited." Yellow
                     }
                 }
             }
 
+            #checking file has been deleted
             foreach($key in $baselineDictionary.Keys){
                 if (-not [string]::IsNullOrEmpty($key)){
                     $existence = Test-Path -Path $key
@@ -88,7 +106,9 @@ while($true){
     }
     else{
         Write-Host ""
-        Write-Host "Select A or B"
+        Write-Host "Select A or B" -ForegroundColor Magenta
         Write-Host ""
     }
 }
+
+pause
